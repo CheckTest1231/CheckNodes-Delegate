f1a57814887de7adb8c084d1415d1e5ac6d152a8
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-17 02:41:12 +0200

    Update CheckCosmos.sh

--- CheckCosmos.sh
+++ CheckCosmos.sh
@@ -1,7 +1,7 @@
 User
 #!/bin/bash
 
-# Функция для вывода зеленого текста
+
 printGreen() {
   echo -e "\e[32m$1\e[0m"
 }
@@ -10,7 +10,7 @@ function printDelimiter {
   echo "==========================================="
 }
 
-# Функция для проверки ноды и вывода журнала логов
+
 checkNode() {
   node_name=$1
   node_command=$2
@@ -20,18 +20,15 @@ checkNode() {
   printDelimiter
   echo ""
 
-  # Запускаем команду для проверки ноды и ждем 5 секунд
+  
   timeout 5 sudo journalctl -u $node_command -f -o cat
 }
 
-# Проверка ноды Lava
-checkNode "Lava" "lava"
 
-# Проверка ноды Zetachain
+checkNode "Lava" "lavad"
+
 checkNode "Zetachain" "zetacored"
 
-# Проверка ноды Babylond
 checkNode "Babylond" "babylond"
 
-# Проверка ноды Dymension
 checkNode "Dymension" "dymd"
