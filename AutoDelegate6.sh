168376f9ace6ddac3ac6986346704b0278f254ce
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-26 13:20:33 +0200

    Update and rename AutoDelegate4.sh to AutoDelegate6.sh

--- AutoDelegate6.sh
+++ AutoDelegate6.sh
@@ -24,8 +24,6 @@ wget -P /root/AUTODELEGATE/ -N \
 wget -P /root/AUTODELEGATE/ -N \
  	https://github.com/CryptoManUA/auto-delegate-cosmos/raw/main/DelegDymension.sh
 
-# Встановлюємо редактор nano за замовчуванням
 export EDITOR=nano
 
-# Створюємо crontab для користувача root
-{ echo "0 */2 * * * sleep 5 && bash /root/AUTODELEGATE/DelegLava.sh"; echo "0 */2 * * * sleep 20 && bash /root/AUTODELEGATE/DelegZeta.sh"; echo "0 */2 * * * sleep 40 && bash /root/AUTODELEGATE/DelegDymension.sh"; } | sudo crontab -
+{ crontab -l; echo "0 */2 * * * sleep 5 && bash /root/AUTODELEGATE/DelegLava.sh"; echo "0 */2 * * * sleep 20 && bash /root/AUTODELEGATE/DelegZeta.sh"; echo "0 */2 * * * sleep 40 && bash /root/AUTODELEGATE/DelegDymension.sh"; } | crontab -
