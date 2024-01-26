9ce1c2bc97a84b8b64937af1708cdc8f18ad6554
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-26 13:52:42 +0200

    Update and rename AutoDelegate.sh to AutoDelegate1.sh

--- AutoDelegate1.sh
+++ AutoDelegate1.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+
 function logo() {
     bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 }
 
+
 logo
+
 
 sudo apt update
 sudo apt install -y cron
+
 
 echo "export PATH=\$PATH:/root/AUTODELEGATE/" >> ~/.profile
 echo "export PATH=\$PATH:/root/AUTODELEGATE/" >> ~/.bash_profile
@@ -15,7 +19,9 @@ echo "export PATH=\$PATH:/root/AUTODELEGATE/" >> ~/.bash_profile
 sudo systemctl enable cron.service
 sudo systemctl start cron.service
 
+
 mkdir -p /root/AUTODELEGATE/
+
 
 wget -P /root/AUTODELEGATE/ -N \
 	https://github.com/CryptoManUA/auto-delegate-cosmos/raw/main/DelegLava.sh
@@ -23,10 +29,23 @@ wget -P /root/AUTODELEGATE/ -N \
  	https://github.com/CryptoManUA/auto-delegate-cosmos/raw/main/DelegZeta.sh
 wget -P /root/AUTODELEGATE/ -N \
  	https://github.com/CryptoManUA/auto-delegate-cosmos/raw/main/DelegDymension.sh
-  
-chmod +x $HOME/AUTODELEGATE/DelegLava.sh
-chmod +x $HOME/AUTODELEGATE/DelegZeta.sh
-chmod +x $HOME/AUTODELEGATE/DelegDymension.sh
+
+
+chmod +x /root/AUTODELEGATE/DelegLava.sh
+chmod +x /root/AUTODELEGATE/DelegZeta.sh
+chmod +x /root/AUTODELEGATE/DelegDymension.sh
+
 export EDITOR=nano
 
-{ echo "0 */2 * * * sleep 5 && bash /root/AUTODELEGATE/DelegLava.sh"; echo "0 */2 * * * sleep 20 && bash /root/AUTODELEGATE/DelegZeta.sh"; echo "0 */2 * * * sleep 40 && bash /root/AUTODELEGATE/DelegDymension.sh"; } | sudo crontab -
+
+if [ ! -f /root/AUTODELEGATE/first_run_completed ]; then
+    
+    bash /root/AUTODELEGATE/DelegLava.sh
+    bash /root/AUTODELEGATE/DelegZeta.sh
+    bash /root/AUTODELEGATE/DelegDymension.sh
+
+    
+    touch /root/AUTODELEGATE/first_run_completed
+fi
+
+{ echo "0 */2 * * * bash /root/AUTODELEGATE/DelegLava.sh"; echo "0 */2 * * * sleep 15 && bash /root/AUTODELEGATE/DelegZeta.sh"; echo "0 */2 * * * sleep 30 && bash /root/AUTODELEGATE/DelegDymension.sh"; } | sudo crontab -
