731371d4e88942eb32cc8061238239a4ba36dfc9
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-27 13:52:00 +0200

    Update and rename AutoDelegate.sh to AutoDelegate1.sh

--- AutoDelegate1.sh
+++ AutoDelegate1.sh
@@ -33,9 +33,10 @@ wget -P /root/AUTODELEGATE/ -N \
   echo "* */2 * * * sleep 5 bash /root/AUTODELEGATE/DelegLava.sh"
   echo "* */2 * * * sleep 20 && bash /root/AUTODELEGATE/DelegZeta.sh"
   echo "* */2 * * * sleep 40 && bash /root/AUTODELEGATE/DelegDymension.sh"
-  echo "0 */12 * * * echo \"tokens: \$(/root/go/bin/lavad q staking validator \$(/root/go/bin/lavad keys show wallet --bech val -a) | grep -E 'tokens') - \$(date)\" >> /root/AUTODELEGATE/Lava.txt"
-  echo "0 */12 * * * echo \"tokens: \$(/root/go/bin/zetacored q staking validator \$(/root/go/bin/zetacored keys show wallet --bech val -a) | grep -E 'tokens') - \$(date)\" >> /root/AUTODELEGATE/Zeta.txt"
-  echo "0 */12 * * * echo \"tokens: \$(/root/go/bin/dymd q staking validator \$(/root/go/bin/dymd keys show wallet --bech val -a) | grep -E 'tokens') - \$(date)\" >> /root/AUTODELEGATE/Dymension.txt"
+  0 */2 * * * sleep 10 && /root/go/bin/lavad q staking validator $(/root/go/bin/lavad keys show wallet --bech val -a) | grep -E "tokens" | while IFS= read -r line; do echo "$(date +%Y-%m-%d\ %H:%M:%S) $line"; done >> /root/AUTODELEGATE/Lava.txt
+  0 */2 * * * sleep 30 && /root/go/bin/zetacored q staking validator $(/root/go/bin/zetacored keys show wallet --bech val -a) | grep -E "tokens" | while IFS= read -r line; do echo "$(date +%Y-%m-%d\ %H:%M:%S) $line"; done >> /root/AUTODELEGATE/Zeta.txt
+  0 */2 * * * sleep 50 && /root/go/bin/dymd q staking validator $(/root/go/bin/dymd keys show wallet --bech val -a) | grep -E "tokens" | while IFS= read -r line; do echo "$(date +%Y-%m-%d
+
 
 
 } | crontab -
