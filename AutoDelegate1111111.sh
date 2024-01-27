2d803b3f443def6af0883d6e7ab2d63e714e5bd2
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-27 14:29:08 +0200

    Update and rename AutoDelegate111111.sh to AutoDelegate1111111.sh

--- AutoDelegate1111111.sh
+++ AutoDelegate1111111.sh
@@ -8,6 +8,7 @@ logo
 
 sudo apt update
 sudo apt install -y cron
+sudo apt install -y language-pack-uk
 
 echo "export PATH=\$PATH:/root/AUTODELEGATE/" >> ~/.profile
 echo "export PATH=\$PATH:/root/AUTODELEGATE/" >> ~/.bash_profile
@@ -25,12 +26,13 @@ wget -P /root/AUTODELEGATE/ -N \
     https://github.com/CryptoManUA/auto-delegate-cosmos/raw/main/DelegDymension.sh
 
 {
+  echo "LC_TIME=uk_UA.UTF-8"
   echo "0 */2 * * * sleep 5 && bash /root/AUTODELEGATE/DelegLava.sh"
   echo "0 */2 * * * sleep 20 && bash /root/AUTODELEGATE/DelegZeta.sh"
   echo "0 */2 * * * sleep 40 && bash /root/AUTODELEGATE/DelegDymension.sh"
-  echo "0 */2 * * * sleep 10 && /root/go/bin/lavad q staking validator \$(/root/go/bin/lavad keys show wallet --bech val -a) | grep -E \"tokens\" | while IFS= read -r line; do echo \"tokens: \$line - \$(date '+%Y-%m-%d %H:%M:%S')\"; done >> /root/AUTODELEGATE/Lava.txt"
-  echo "0 */2 * * * sleep 30 && /root/go/bin/zetacored q staking validator \$(/root/go/bin/zetacored keys show wallet --bech val -a) | grep -E \"tokens\" | while IFS= read -r line; do echo \"tokens: \$line - \$(date '+%Y-%m-%d %H:%M:%S')\"; done >> /root/AUTODELEGATE/Zeta.txt"
-  echo "0 */2 * * * sleep 50 && /root/go/bin/dymd q staking validator \$(/root/go/bin/dymd keys show wallet --bech val -a) | grep -E \"tokens\" | while IFS= read -r line; do echo \"tokens: \$line - \$(date '+%Y-%m-%d %H:%M:%S')\"; done >> /root/AUTODELEGATE/Dymension.txt"
+  echo "0 */2 * * * LC_TIME=uk_UA.UTF-8 /root/go/bin/lavad q staking validator \$(/root/go/bin/lavad keys show wallet --bech val -a) | grep -E \"tokens\" | while IFS= read -r line; do echo \"tokens: \$line - \$(LC_TIME=uk_UA.UTF-8 date '+%Y-%m-%d %H:%M:%S')\"; done >> /root/AUTODELEGATE/Lava.txt"
+  echo "0 */2 * * * LC_TIME=uk_UA.UTF-8 /root/go/bin/zetacored q staking validator \$(/root/go/bin/zetacored keys show wallet --bech val -a) | grep -E \"tokens\" | while IFS= read -r line; do echo \"tokens: \$line - \$(LC_TIME=uk_UA.UTF-8 date '+%Y-%m-%d %H:%M:%S')\"; done >> /root/AUTODELEGATE/Zeta.txt"
+  echo "0 */2 * * * LC_TIME=uk_UA.UTF-8 /root/go/bin/dymd q staking validator \$(/root/go/bin/dymd keys show wallet --bech val -a) | grep -E \"tokens\" | while IFS= read -r line; do echo \"tokens: \$line - \$(LC_TIME=uk_UA.UTF-8 date '+%Y-%m-%d %H:%M:%S')\"; done >> /root/AUTODELEGATE/Dymension.txt"
 } | crontab -
 
 echo "Cron розклад успішно створений."
