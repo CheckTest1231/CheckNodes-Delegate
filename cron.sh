8ee8631f694f9628aa6e9be67c72be6f1bea7bb3
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-27 13:17:58 +0200

    Create cron.sh

---
+++ cron.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
+echo ""
+printGreen "Останні 15 записів файлу Lava.txt"
+tail -n 15 $HOME/AUTODELEGATE/Lava.txt
+echo ""
+printGreen "Останні 15 записів файлу Zeta.txt"
+tail -n 15 $HOME/AUTODELEGATE/Zeta.txt
+echo ""
+printGreen "Останні 15 записів файлу Dymension.txt"
+tail -n 15 $HOME/AUTODELEGATE/Dymension.txt
+echo ""
