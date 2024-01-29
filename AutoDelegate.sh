25c3ecb0a6f23a0aec9672f10e34b6894e6da161
CheckTest1231 <142440040+CheckTest1231@users.noreply.github.com>
2024-01-29 13:31:39 +0200

    Update and rename AutoDelegatee.sh to AutoDelegate.sh

---
+++ AutoDelegate.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+function logo() {
+    bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
+}
+
+function printGreen {
+  echo -e "\e[1m\e[32m${1}\e[0m"
+}
+
+function printDelimiter {
+  echo "==========================================="
+}
+
+clear
+logo
+printGreen "Встановлення автоматичного делегування токенів нод: Lava, Zetachain, Dymension"
+sudo apt update
+sudo apt install -y cron
+
+echo "export PATH=\$PATH:/root/AUTODELEGATE/" >> ~/.profile
+echo "export PATH=\$PATH:/root/AUTODELEGATE/" >> ~/.bash_profile
+
+sudo systemctl enable cron.service
+sudo systemctl start cron.service
+
+mkdir -p /root/AUTODELEGATE/
+
+wget -P /root/AUTODELEGATE/ -N \
+    https://github.com/CryptoManUA/auto-delegate-cosmos/raw/main/DelegLava.sh
+wget -P /root/AUTODELEGATE/ -N \
+    https://github.com/CryptoManUA/auto-delegate-cosmos/raw/main/DelegZeta.sh
+wget -P /root/AUTODELEGATE/ -N \
+    https://github.com/CryptoManUA/auto-delegate-cosmos/raw/main/DelegDymension.sh
+
+{
+ 0 */2 * * * sleep 5 && bash /root/AUTODELEGATE/DelegLava.sh
+ 0 */2 * * * sleep 20 && bash /root/AUTODELEGATE/DelegZeta.sh
+ 0 */2 * * * sleep 40 && bash /root/AUTODELEGATE/DelegDymension.sh
+ 0 */1 * * * sleep 10 && /root/go/bin/lavad q staking validator $(/root/go/bin/lavad keys show wallet --bech val -a) | grep -E "tokens" >> /root/AUTODELEGATE/Lava.txt
+ 0 */1 * * * sleep 30 && /root/go/bin/zetacored q staking validator $(/root/go/bin/zetacored keys show wallet --bech val -a) | grep -E "tokens" >> /root/AUTODELEGATE/Zeta.txt
+ 0 */1 * * * sleep 50 && /root/go/bin/dymd q staking validator $(/root/go/bin/dymd keys show wallet --bech val -a) | grep -E "tokens" >> /root/AUTODELEGATE/Dymension.txt
+} | crontab -
+
+echo ""
+printDelimiter
+printGreen "Cron розклад успішно створений."
+printGreen "Щоб перевірити роботу скрипта та фактичну кількість токенів заделегованих в вашого валідатора в данний момент - скористайтесь командою:"
+printGreen "checkcron"
+printGreen "Вивід команди: tokens: значення - дата та час останньго занесення інформації"
+printDelimiter
+printGreen "Через 10 секунд виконається тестове виконання скрипта:"
+sleep 1 && /root/go/bin/lavad q staking validator $(/root/go/bin/lavad keys show wallet --bech val -a) | grep -E "tokens" >> /root/AUTODELEGATE/Lava.txt
+sleep 2 && /root/go/bin/zetacored q staking validator $(/root/go/bin/zetacored keys show wallet --bech val -a) | grep -E "tokens" >> /root/AUTODELEGATE/Zeta.txt
+sleep 2 && /root/go/bin/dymd q staking validator $(/root/go/bin/dymd keys show wallet --bech val -a) | grep -E "tokens" >> /root/AUTODELEGATE/Dymension.txt
+bash <(curl -s https://raw.githubusercontent.com/CheckTest1231/CheckNodes-Delegate/main/checkcron.sh)
+exec bash
